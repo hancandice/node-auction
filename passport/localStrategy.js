c3a0029d123a2d444644b9d8a9e4cfe86add0452
Jeeyoung <64334381+hancandice@users.noreply.github.com>
2023-01-12 11:26:58 +0700

    🔥 FEAT: add passport library && make local strategy

---
+++ passport/localStrategy.js
@@ -0,0 +1,35 @@
+const { compare } = require("bcrypt");
+const { use } = require("passport");
+const { Strategy: LocalStrategy } = require("passport-local");
+const User = require("../models/user");
+
+module.exports = () => {
+  use(
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passwordField: "password",
+      },
+      async (email, password, done) => {
+        try {
+          const exUser = await User.findOne({ where: { email } });
+          if (exUser) {
+            const isLoggedIn = await compare(password, exUser.password);
+            if (isLoggedIn) {
+              done(null, exUser);
+            } else {
+              done(null, false, { message: "Password not correct." });
+            }
+          } else {
+            done(null, false, {
+              message: "No matching credentials. Please sign up.",
+            });
+          }
+        } catch (error) {
+          console.log(error);
+          done(error);
+        }
+      }
+    )
+  );
+};
