e4d8dd9055a0f4b635a8dba01bdc4a14aa7b02fe
Jeeyoung <64334381+hancandice@users.noreply.github.com>
2023-01-12 13:25:54 +0700

    👩 FEAT: update app.js

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
 const express = require("express");
+const nunjucks = require("nunjucks");
 const { sequelize } = require("./models");
+const indexRouter = require("./routes/index");
 
 const app = express();
 
 app.set("port", process.env.PORT || 8010);
+app.set("view engine", "html");
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
+});
 
 sequelize
   .sync({ force: false })
   .then(() => {
-    console.log("Database connection success.");
+    console.log("Database connection success. 💾");
   })
   .catch((error) => {
     console.log({ error });
   });
 
+app.use("/", indexRouter);
+
+app.use((req, res, next) => {
+  const routerErr = new Error(`No router for ${req.method} ${req.url} 🥲`);
+  routerErr.status = 404;
+  next(routerErr);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV != "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
 app.listen(app.get("port"), () => {
-  console.log("waiting on port ", app.get("port"));
+  console.log("waiting on port ⛴ ", app.get("port"));
 });
--- passport/localStrategy.js
+++ passport/localStrategy.js
@@ -26,7 +26,7 @@ module.exports = () => {
             });
           }
         } catch (error) {
-          console.log(error);
+          console.log({ error });
           done(error);
         }
       }
