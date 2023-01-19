ec131c94a671d527a171a8ec7b78cc849d8b224a
hancandice <jeeyoung.han82@gmail.com>
2023-01-19 17:00:41 +0700

    😗 FEAT: add auction logic

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const multer = require("multer");
 const path = require("path")
 const fs = require("fs")
-const { Good } = require("../models");
+const { Good, User, Auction } = require("../models");
 const { checkNotLoggedIn, checkLoggedIn } = require("./middlewares");
 
 const router = Router();
@@ -70,7 +70,50 @@ router.post("/good", checkLoggedIn, upload.single("img"), async (req, res, next)
     console.error({ err })
     next(err)
   }
+})
 
+router.get("/good/:id", checkLoggedIn, async (req, res, next) => {
+  try {
+    const good = await Good.findOne({
+      where: { id: req.params.id },
+      include: {
+        model: User,
+        as: "Owner"
+      }
+    })
+    res.render("auction", {
+      title: `${good.name} - Auction`,
+      good
+    })
+  } catch (error) {
+    console.log({ error })
+    next(error)
+  }
+})
+
+router.post("/good/:id/bid", checkLoggedIn, async (req, res, next) => {
+  try {
+    const { bid, msg } = req.body
+    const good = await Good.findOne({
+      where: { id: req.params.id },
+    })
+    if (good.price >= bid) {
+      return res.status(403).send("You must bid above the starting price.")
+    }
+    if (new Date(good.createdAt).valueOf() + (24 * 60 * 60 * 1000) < new Date()) {
+      return res.status(403).send("Auction has already ended.")
+    }
+    const result = await Auction.create({
+      bid,
+      msg,
+      UserId: req.user.id,
+      GoodId: req.params.id
+    })
+    return res.send("ok")
+  } catch (error) {
+    console.error({ error })
+    return next(error)
+  }
 })
 
 module.exports = router;
